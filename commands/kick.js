aa1d7093305d498daf847b918bbfeb7ae77bb2dc
Louis Albano <louisalbano21@gmail.com>
2019-01-20 11:12:31 -0500

    Reorganize command file structure

---
+++ commands/kick.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: 'kick',
+  description: 'Kick their butt in the butt',
+  execute(message, args) {
+    // grab the "first" mentioned user from the message
+    // this will return a `User` object, just like `message.author`
+    const taggedUser = message.mentions.users.first();
+
+    if (!message.mentions.users.size) {
+      return message.reply('you need to tag a user in order to kick them!');
+    }
+
+    message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+  }
+}
--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const { prefix, token } = require('./config.json');
+const fs = require('fs');
 const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+  // set a new item in the Collection
+  // with the key as the command name and the value as the exported module
+  client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
   console.log('Ready!');
@@ -14,57 +27,16 @@ client.on('message', message => {
   const args = message.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
 
-  console.log(command);
-  if (command === 'ping') {
-    message.channel.send('Pong.');
+  if (!client.commands.has(command)) {
+    return;
   }
-  else if (command === 'beep') {
-    message.channel.send('Boop.');
+
+  try {
+    client.commands.get(command).execute(message, args);
   }
-  else if (command === 'server') {
-    message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-  }
-  else if (command === 'args-info') {
-    if (!args.length) {
-      return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-    }
-    else if (args[0] === 'foo') {
-      return message.channel.send('bar');
-    }
-    message.channel.send(`Command name: ${command}\nArguments: ${args}`);
-  }
-  else if (command === 'kick') {
-    // grab the "first" mentioned user from the message
-    // this will return a `User` object, just like `message.author`
-    const taggedUser = message.mentions.users.first();
-
-    if (!message.mentions.users.size) {
-      return message.reply('you need to tag a user in order to kick them!');
-    }
-
-    message.channel.send(`You wanted to kick: ${taggedUser.username}`);
-  }
-  else if (command === 'avatar') {
-    if (!message.mentions.users.size) {
-      return message.channel.send(`Your avatar: <${message.author.displayAvatarURL}>`);
-    }
-
-    const avatarList = message.mentions.users.map(user => {
-      return `${user.username}'s avatar: <${user.displayAvatarURL}>`;
-    });
-
-    // send the entire array of strings as a message
-    // by default, discord.js will `.join()` the array with `\n`
-    message.channel.send(avatarList);
-  }
-  else if (command === 'prune') {
-    const amount = parseInt(args[0]);
-
-    if (isNaN(amount)) {
-      return message.reply('that doesn\'t seem to be a valid number.');
-    }
-
-    // ...
+  catch (error) {
+    console.error(error);
+    message.reply('Something done broke');
   }
 });
 
